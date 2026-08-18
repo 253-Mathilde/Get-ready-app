332accf17af5cdc332eea8b3e6031909f6c5b6e8
253-Mathilde <arianebaby08@gmail.com>
2026-08-18 18:37:36 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 document.getElementById("animal").addEventListener("click", function() { 
     window.location.href = "create1.html";
  });
+
+ document.body.addEventListener("click", function() {
+    if (event.target === document.body) {
+         window.location.href = "index.html";
+    }
+});
