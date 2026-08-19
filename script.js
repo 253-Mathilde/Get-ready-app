08cfaeae26805c988a5513bc443696d9333ec936
253-Mathilde <arianebaby08@gmail.com>
2026-08-19 21:42:50 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 
+const vid = document.querySelector("#vd");
+vid.muted = true;
+vid.play();
+
+vid.addEventListener("ended", () => {
+loader.remove();
+vid.remove();
+});
+
+
 document.getElementById("create").addEventListener("click", function() { 
     window.location.href = "create1.html";
  });
