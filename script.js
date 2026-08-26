b479f676f346db727c8d93b58e4dd37d9270dbe7
253-Mathilde <arianebaby08@gmail.com>
2026-08-26 22:05:03 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
+
 const loader = document.querySelector("#loader");
 const vid = document.querySelector("#vd");
 
-// 1. VIDEO-LOGIK
-// Wird nur ausgeführt, wenn loader und vd auf dem aktuellen Screen existieren (also auf index.html)
 if (loader && vid) {
     if (sessionStorage.getItem("videoPlayed")) {
         loader.remove();
@@ -20,20 +19,17 @@ if (loader && vid) {
     }
 }
 
-// 2. BUTTON ZUR UNTERSEITE ("create")
+
 const createBtn = document.getElementById("create");
 if (createBtn) {
     createBtn.addEventListener("click", function() { 
-        // Absoluter Pfad für deine GitHub-Pages Domain (Wechsel in den Unterordner)
         window.location.href = "/Get-ready-app/screenone_arrivetime/create1.html";
     });
 }
 
-// 3. HOME-BUTTON ("text")
 const textBtn = document.getElementById("text");
 if (textBtn) {
     textBtn.addEventListener("click", function() { 
-        // Absoluter Pfad zurück zur Startseite (Hauptverzeichnis)
         window.location.href = "/Get-ready-app/index.html";
     });
 }
