33c38a20f04604478ab78dc544e32c72124691e8
253-Mathilde <arianebaby08@gmail.com>
2026-08-26 21:33:34 +0200

    Fix redirection path in script.js
    Updated the redirection path for the 'text' button.

--- script.js
+++ script.js
@@ -26,5 +26,5 @@ document.getElementById("create").addEventListener("click", function() {
  
 
  document.getElementById("text").addEventListener("click", function() { 
-    window.location.href = "index.html";
+    window.location.href = "../index.html";
  });
