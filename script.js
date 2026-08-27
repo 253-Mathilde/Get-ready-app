624b2404c518cf1cf52c9362ce4e3675aa21b223
253-Mathilde <arianebaby08@gmail.com>
2026-08-27 20:33:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,13 +15,22 @@ if (textBtn) {
 
 
 function nextsite() {
-  const aktuellerPfad = window.location.pathname;
+    const aktuellerPfad = window.location.pathname;
 
-  if (aktuellerPfad.includes("screenone_arrivetime/create1.html")){
-    window.location.href = "/screentwo_waytime/time1.html";
+    if (aktuellerPfad.includes("create1.html")) {
+        // Findet heraus, wo wir gerade sind, und ersetzt nur das Ende der Adresse
+        // Das funktioniert überall, da es sich an der aktuellen URL orientiert
+        let basisPfad = window.location.href.replace("screenone_arrivetime/create1.html", "");
+        window.location.href = basisPfad + "screentwo_waytime/time1.html";
 
-  }else {
-     window.location.href = "index.html";
-  }
+    } else {
+        // Falls du von der Startseite kommst und zu create1.html willst
+        let basisPfad = window.location.href.replace("index.html", "");
+        // Falls am Ende kein Slash ist (passiert manchmal bei Apps), fügen wir einen hinzu
+        if (!basisPfad.endsWith("/")) {
+            basisPfad += "/";
+        }
+        window.location.href = basisPfad + "screenone_arrivetime/create1.html";
+    }
 }
 
