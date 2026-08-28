4578a548e6c4ce8e2113521aa4532d3514676a82
253-Mathilde <arianebaby08@gmail.com>
2026-08-28 21:26:34 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const createBtn = document.getElementById("create");
 if (createBtn) {
     createBtn.addEventListener("click", function() { 
@@ -9,30 +10,39 @@ if (createBtn) {
 const textBtn = document.getElementById("text");
 if (textBtn) {
     textBtn.addEventListener("click", function() { 
-        // Springt aus dem Unterordner eine Ebene höher zurück zur index.html
         window.location.href = "../index.html";
     });
 }
-
-
 
 function nextsite() {
     const aktuellerPfad = window.location.pathname;
 
     if (aktuellerPfad.includes("create1.html")) {
-        // Findet heraus, wo wir gerade sind, und ersetzt nur das Ende der Adresse
-        // Das funktioniert überall, da es sich an der aktuellen URL orientiert
         let basisPfad = window.location.href.replace("screenone_arrivetime/create1.html", "");
         window.location.href = basisPfad + "screentwo_waytime/time1.html";
 
     } else {
-        // Falls du von der Startseite kommst und zu create1.html willst
         let basisPfad = window.location.href.replace("index.html", "");
-        // Falls am Ende kein Slash ist (passiert manchmal bei Apps), fügen wir einen hinzu
-        if (!basisPfad.endsWith("/")) {
+    if (!basisPfad.endsWith("/")) {
             basisPfad += "/";
         }
         window.location.href = basisPfad + "screenone_arrivetime/create1.html";
     }
 }
+const arrivetime1 = Number(document.getElementById("hour")?.value || 0)* 60;
+const arrivetime2 = Number(document.getElementById("minutes")?.value || 0);
+const summe1 = arrivetime1 + arrivetime2;
+console.log("Summe 1:", summe1); 
 
+const waytime1 = Number(document.getElementById("hourfortheway")?.value || 0)* 60;
+const waytime2 = Number(document.getElementById("minutesfortheway")?.value || 0);
+const summe2 = waytime1 + waytime2;
+
+const summezeit = summe1 - summe2;
+console.log("Endergebnis:", summezeit);
+
+
+
+const endStunden = Math.floor(ergebnis_in_minuten / 60);
+const endMinuten = ergebnis_in_minuten % 60;
+console.log(`Ergebnis: ${endStunden} Stunden und ${endMinuten} Minuten`);
