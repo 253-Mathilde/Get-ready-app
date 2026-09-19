b9226e683ce5195a90459a6db6f3ed6983840647
253-Mathilde <arianebaby08@gmail.com>
2026-09-19 15:43:21 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1405,8 +1405,10 @@ if (aktuellerPfad.includes("screen_for_the_actual-mode_three/whatclotheswearsthe
     const note = localStorage.getItem("notestext");
     const text = document.getElementById('timetext');
     if (text) {
-        text.textContent = `${note}`;
+        text.textContent = note ||"Looks like you didn't leave yourself a note! But I'm here for you ,so...Wish you the best for your event!";
+        
     }
+
 
 
 
