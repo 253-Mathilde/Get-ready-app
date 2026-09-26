a79cda2b7465c051ab7466838feafe85562d3ccd
253-Mathilde <arianebaby08@gmail.com>
2026-09-26 15:09:05 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -124,6 +124,14 @@ function nextsite() {
 
         }
 
+        else if (aktuellerPfad.includes("extrasite.html")) {
+            let basisPfad = window.location.href.replace("screen_for_the-actual_mode_five/extrasite.html", ""); 
+            localStorage.setItem('lastVisitedPage',basisPfad + "screen_for_the-actual_mode_six" );
+            window.location.href = basisPfad + "screen_for_the-actual_mode_six"; 
+            
+
+        }
+
         else {
           console.log("nextsite didn't work");
         }
